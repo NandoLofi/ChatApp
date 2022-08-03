3f4387f4ef2a7453ec8555ad006f55d0602c7c44
Fernando Galvan <n.brighter00@gmail.com>
2022-08-02 19:25:12 -0500

    added components and css functionality

--- chatapp/src/Sidebar.jsx
+++ chatapp/src/Sidebar.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import './Sidebar.css'
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import { IconButton, Avatar } from '@mui/material';
+
+
+
 export default function Sidebar() {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+      <div className="sidebar__header">
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon/>
+          </IconButton>
+        </div>
+      </div>
+    </div>
   )
 }
