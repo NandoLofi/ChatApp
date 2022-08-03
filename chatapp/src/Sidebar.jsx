1e29a1e8a192ec208095584b744c1ddc3f3427a9
Fernando Galvan <n.brighter00@gmail.com>
2022-08-02 19:37:39 -0500

    added material.ui and icons

--- chatapp/src/Sidebar.jsx
+++ chatapp/src/Sidebar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Sidebar.css'
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
-import { IconButton, Avatar } from '@mui/material';
+import { IconButton, Avatar, Icon } from '@mui/material';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 
 
@@ -9,9 +11,16 @@ export default function Sidebar() {
   return (
     <div className='sidebar'>
       <div className="sidebar__header">
+        <Avatar src='https://external-preview.redd.it/Of20Cd3_f3Jep2nEaeTUOtElG4l7B8HdBGWYEQdOh5c.png?format=pjpg&auto=webp&s=1e4fd354212a32ba81ea9c8eb46be922c3ee089b'/>
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon/>
+          </IconButton>
+          <IconButton>
+            <ChatBubbleOutlineIcon/>
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon/>
           </IconButton>
         </div>
       </div>
