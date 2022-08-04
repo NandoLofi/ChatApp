7951dea904ed5446b9991487234ea50faa30c218
Fernando Galvan <n.brighter00@gmail.com>
2022-08-03 20:00:42 -0500

    fixed small bug

--- chatapp-mern/chatapp/src/Chat.jsx
+++ chatapp-mern/chatapp/src/Chat.jsx
@@ -4,7 +4,7 @@ import { Avatar, IconButton } from "@mui/material"
 import { SearchOutlined, MoreVert, AttachFile, InsertEmoticon } from '@mui/icons-material';
 import MicIcon from '@mui/icons-material/Mic';
 
-export default function Chat( { messages } ) {
+export default function Chat({ messages }) {
   return (
     <div className='chat'>
       <div className="chat__header">
@@ -27,14 +27,13 @@ export default function Chat( { messages } ) {
         </div>
       </div> 
       <div className="chat__body">
-        {messages.map(message => {
+        {messages.map((message)=> (
            <p className={`chat__message ${message.revieved && "chat__reciever"}`}>
            <span className="chat__name">{message.name}</span>
              {message.message}
            <span className="chat__timestamp"> {message.timestamp}</span>
          </p>
-        })}
-  
+        ))}
       </div>
       <div className="chat__footer">
         <InsertEmoticon />
