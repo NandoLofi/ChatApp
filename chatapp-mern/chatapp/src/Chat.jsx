204f8f0eb39959548f9e93e23cfc6fe739b928bb
Fernando Galvan <n.brighter00@gmail.com>
2022-08-03 21:02:00 -0500

    fully functional app but needs further tech

--- chatapp-mern/chatapp/src/Chat.jsx
+++ chatapp-mern/chatapp/src/Chat.jsx
@@ -1,11 +1,22 @@
 import {React, useState} from 'react'
 import './Chat.css'
 import { Avatar, IconButton } from "@mui/material"
-import { SearchOutlined, MoreVert, AttachFile, InsertEmoticon } from '@mui/icons-material';
+import { SearchOutlined, MoreVert, AttachFile, InsertEmoticon, SettingsInputAntenna, AutoFixHighSharp } from '@mui/icons-material';
 import MicIcon from '@mui/icons-material/Mic';
+import axios from './axios'
 
 export default function Chat({ messages }) {
   const [input, setInput] = useState("")
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    axios.post('/messages/new', {
+      message: input,
+      name: "ChatApp",
+      timestamp: "Just Now",
+      recieved: false
+    });
+    setInput("")
+  }
   return (
     <div className='chat'>
       <div className="chat__header">
@@ -29,9 +40,11 @@ export default function Chat({ messages }) {
       </div> 
       <div className="chat__body">
         {messages.map((message)=> (
-           <p className={`chat__message ${message.recieved && "chat_reciever"}`}>
+           <p 
+           className={`chat__message ${message.recieved && "chat__reciever"}`}
+           >
            <span className="chat__name">{message.name}</span>
-             {message.message}
+            {message.message}
            <span className="chat__timestamp"> {message.timestamp}</span>
          </p>
         ))}
@@ -39,8 +52,8 @@ export default function Chat({ messages }) {
       <div className="chat__footer">
         <InsertEmoticon />
         <form>
-          <input placeholder='Type a message' type="text"/>
-            <button>
+          <input value={input} onChange={(e)=> setInput(e.target.value)} placeholder='Type a message' type="text"/>
+            <button onClick={sendMessage} type="submit">
               Send a message
             </button>
         </form>
